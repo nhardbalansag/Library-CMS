ad424c2a0ed925cbd836811baf4280d701aaa6d4
nhardbalansag <nhardbalansag@gmail.com>
2021-02-16 22:59:21 +0800

    fix admin error

--- routes/web.php
+++ routes/web.php
@@ -59,6 +59,7 @@ Route::group(['middleware' => 'auth'], function () {
     });
 });
 
+
 Route::group(['middleware' => 'auth'], function () {
 	Route::get('profile', ['as' => 'profile.edit', 'uses' => 'App\Http\Controllers\ProfileController@edit']);
 	Route::put('profile', ['as' => 'profile.update', 'uses' => 'App\Http\Controllers\ProfileController@update']);
