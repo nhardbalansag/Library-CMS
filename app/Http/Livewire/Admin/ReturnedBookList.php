7def3e73844f39413135b6fad3477109136b12fd
nhardbalansag <nhardbalansag@gmail.com>
2021-02-19 13:49:38 +0800

    fix

--- app/Http/Livewire/Admin/ReturnedBookList.php
+++ app/Http/Livewire/Admin/ReturnedBookList.php
@@ -36,7 +36,7 @@ class ReturnedBookList extends Component
         $data = DB::table('borrow_books')
                 ->join('books', 'books.id', '=', 'borrow_books.book_id')
                 ->join('book_categories', 'book_categories.id', '=', 'books.BookCategoryId')
-                ->select('books.*', 'book_categories.title as bookCategoryTitle','borrow_books.id as borrowId', 'borrow_books.status as borrowStatus', 'borrow_books.created_at as dateBorrowed', 'borrow_books.returnDate as returnDate')
+                ->select('books.*','book_categories.updated_at as returnbookDate',  'book_categories.title as bookCategoryTitle','borrow_books.id as borrowId', 'borrow_books.status as borrowStatus', 'borrow_books.created_at as dateBorrowed', 'borrow_books.returnDate as returnDate')
                 ->where('books.title', 'like', '%' . $this->booktitle . '%')
                 ->where('borrow_books.status', 'return')
                 ->paginate(10);
